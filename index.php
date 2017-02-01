1c81148609bc2a3f0d87948b3f7c67f22baee6ba
dsb12051 <dbroe1995@gmail.com>
2017-02-01 02:54:18 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<title>JD Weather Station</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	<div id="info">
+		<div id="largeBold1">[insert name]'s Weather Station</div>
+		<span id="smallFont1">Randolph, VT</span>
+	</div>
+	<div id="sidebar">
+		Pressure: 29.85 in<br>
+		<br>
+		<br>
+		Humidity: 72%<br>
+		<br>
+		<br>
+		Wind Speed: 0 mph<br>
+		<br>
+		<br>
+		Wind Direction: South<br>
+		<br>
+		<br>
+		Rainfall: 0.00 in<br>
+		<br>
+		<br>
+		Sunrise: 7:07 am<br>
+		<br>
+		<br>
+		Sunset: 5:01 pm
+	</div>
+	<div id="temperature">
+		<span id="largeBold2">Current Temperature:</span>
+		<div id="largeBold3">17.4 °F</div>
+		<br>
+		<span id="largeBold4a">High: 26.4 °F</span>
+		<span id="largeBold4b">Low: 3.2 °F</span><br>
+		<br>
+		<br>
+		Last updated at 10:45 pm
+	</div>
+	<div id="graph">
+		<div id="padding1"><img src="graph.png" width="900" height="115"></div>
+	</div>
+	<div id="loginButton">
+		<form action="login.php">
+			<button id="loginButtonSize">Log in</button>
+		</form>
+	</div>
+</body>
+</html>
